af5b6bb3d59d102a30aceb130196ff93229dd206
KaIakaua <KalakauaOdin@gmail.com>
2026-04-22 00:00:59 -0400

    Added link to tv shows I have watched.

---
+++ config.js
@@ -0,0 +1,10 @@
+const GOOGLE_FORM_ID = "1FAIpQLSey6O-yqBfPfyNGOcAT4MpftMcVRmfG44P2-L4ouSK1pXYnQw";
+const ENTRY_ID = "entry.86254164";
+const SOURCE_ENTRY_ID = "entry.174567179";
+const SECRET_CODE = "dickpoop";
+const GOOGLE_SHEET_ID = "16DarafIA0EjKZlcCyvyCHP_V_86MVZnqhqz5toxnFMQ";
+const DISPLAY_IMAGES = true;
+
+const MSG_FORM_ID = "1FAIpQLSd9o6QRP5i8_mvppcbGE3VOwBUvBtoSgRH0YKW_3lQvcWlrdw";
+const MSG_ENTRY_ID = "entry.518003348";
+const MSG_SHEET_ID = "18zYGzUi1lQ3H-V9wgthUCkRMIRQUEYwfXmQxsW4Z0e4";
